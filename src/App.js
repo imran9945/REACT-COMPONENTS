675701fd2d8c61891c293be2a8afda68f0805081
imran9945 <62025696+imran9945@users.noreply.github.com>
2020-07-25 00:58:52 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Test from './test.js';
+function App() {
+  return (
+    <Test tname="linenum1" stname="nam"/>
+  );
+}
+
+export default App;
---
+++ src/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Test(props){
+    return(
+        <div>
+    <h1>THIS IS THE FIRST TEST APP {props.tname}</h1>
+    <hr/>
+    <h1>your name is imran</h1>
+    
+    </div>
+    )
+}
+export default Test;
