74f67eb99e1798c360552171f549f3e3591460a7
imran9945 <62025696+imran9945@users.noreply.github.com>
2020-07-25 12:43:33 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Test from './test.js';
 function App() {
   return (
-    <Test tname="SIR AMIR PINGER" stname="Imran"/>
+    <Test tname="SIR AMIR PINGER" stname="Muhammad Imran"/>
   );
 }
 
